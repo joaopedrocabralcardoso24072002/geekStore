e9a0eb6dfd1e3384bf62aa0ac1d14aa50e21d219
Pedro Watermann <pedro.programmer.pt@gmail.com>
2024-11-12 15:12:29 -0300

    Banco de dados finalizado

--- frmMenu.Designer.cs
+++ frmMenu.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.btnFechar = new System.Windows.Forms.Button();
-            this.mnsMenu = new System.Windows.Forms.MenuStrip();
+            this.strMenu = new System.Windows.Forms.MenuStrip();
             this.cadastroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.clientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.produtosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -37,7 +37,7 @@
             this.demonstrativoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.mnsMenu.SuspendLayout();
+            this.strMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnFechar
@@ -52,7 +52,7 @@
             this.btnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnFechar.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnFechar.ForeColor = System.Drawing.Color.White;
-            this.btnFechar.Location = new System.Drawing.Point(1830, 1026);
+            this.btnFechar.Location = new System.Drawing.Point(1830, 736);
             this.btnFechar.Name = "btnFechar";
             this.btnFechar.Size = new System.Drawing.Size(78, 42);
             this.btnFechar.TabIndex = 0;
@@ -60,25 +60,28 @@
             this.btnFechar.UseVisualStyleBackColor = false;
             this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
             // 
-            // mnsMenu
+            // strMenu
             // 
-            this.mnsMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.strMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(11)))), ((int)(((byte)(34)))));
+            this.strMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.cadastroToolStripMenuItem,
             this.vendasToolStripMenuItem,
             this.demonstrativoToolStripMenuItem,
             this.sobreToolStripMenuItem,
             this.sairToolStripMenuItem});
-            this.mnsMenu.Location = new System.Drawing.Point(0, 0);
-            this.mnsMenu.Name = "mnsMenu";
-            this.mnsMenu.Size = new System.Drawing.Size(1920, 24);
-            this.mnsMenu.TabIndex = 1;
-            this.mnsMenu.Text = "menuStrip1";
+            this.strMenu.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.strMenu.Location = new System.Drawing.Point(0, 0);
+            this.strMenu.Name = "strMenu";
+            this.strMenu.Size = new System.Drawing.Size(1920, 24);
+            this.strMenu.TabIndex = 1;
+            this.strMenu.Text = "menuStrip1";
             // 
             // cadastroToolStripMenuItem
             // 
             this.cadastroToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.clientesToolStripMenuItem,
             this.produtosToolStripMenuItem});
+            this.cadastroToolStripMenuItem.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.cadastroToolStripMenuItem.Name = "cadastroToolStripMenuItem";
             this.cadastroToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
             this.cadastroToolStripMenuItem.Text = "Cadastro";
@@ -86,56 +89,61 @@
             // clientesToolStripMenuItem
             // 
             this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
-            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
             this.clientesToolStripMenuItem.Text = "Clientes";
             // 
             // produtosToolStripMenuItem
             // 
             this.produtosToolStripMenuItem.Name = "produtosToolStripMenuItem";
-            this.produtosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.produtosToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
             this.produtosToolStripMenuItem.Text = "Produtos";
             // 
             // vendasToolStripMenuItem
             // 
+            this.vendasToolStripMenuItem.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.vendasToolStripMenuItem.Name = "vendasToolStripMenuItem";
             this.vendasToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
             this.vendasToolStripMenuItem.Text = "Vendas";
             // 
             // demonstrativoToolStripMenuItem
             // 
+            this.demonstrativoToolStripMenuItem.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.demonstrativoToolStripMenuItem.Name = "demonstrativoToolStripMenuItem";
             this.demonstrativoToolStripMenuItem.Size = new System.Drawing.Size(97, 20);
             this.demonstrativoToolStripMenuItem.Text = "Demonstrativo";
             // 
             // sobreToolStripMenuItem
             // 
+            this.sobreToolStripMenuItem.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
             this.sobreToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
             this.sobreToolStripMenuItem.Text = "Sobre";
             // 
             // sairToolStripMenuItem
             // 
+            this.sairToolStripMenuItem.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
             this.sairToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
             this.sairToolStripMenuItem.Text = "Sair";
-            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.btnFechar_Click);
             // 
             // frmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DimGray;
-            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(11)))), ((int)(((byte)(34)))));
+            this.ClientSize = new System.Drawing.Size(1920, 790);
             this.Controls.Add(this.btnFechar);
-            this.Controls.Add(this.mnsMenu);
+            this.Controls.Add(this.strMenu);
+            this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.MainMenuStrip = this.mnsMenu;
+            this.MainMenuStrip = this.strMenu;
             this.Name = "frmMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmMenu";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.mnsMenu.ResumeLayout(false);
-            this.mnsMenu.PerformLayout();
+            this.strMenu.ResumeLayout(false);
+            this.strMenu.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,7 +152,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnFechar;
-        private System.Windows.Forms.MenuStrip mnsMenu;
+        private System.Windows.Forms.MenuStrip strMenu;
         private System.Windows.Forms.ToolStripMenuItem cadastroToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem produtosToolStripMenuItem;
--- frmMenu.cs
+++ frmMenu.cs
@@ -14,43 +14,21 @@ namespace geekStore
 {
     public partial class frmMenu : Form
     {
-        public frmMenu(/*bool showSplash = true*/)
+        public frmMenu(bool showSplash = true)
         {
             InitializeComponent();
-            this.Paint += new PaintEventHandler(gradientBg);
 
-            /*if (showSplash)
+            if (showSplash)
             {
                 frmSplash splash = new frmSplash();
                 splash.Show();
                 Application.DoEvents();
-                Thread.Sleep(3000);
+                Thread.Sleep(/*3000*/1000);
                 splash.Close();
-            }*/
-        }
-
-        public void gradientBg(object sender, PaintEventArgs e)
-        {
-            Graphics g = e.Graphics;
-            Rectangle rec = this.ClientRectangle;
-            Color start = ColorTranslator.FromHtml("#B326FF");
-            Color end = ColorTranslator.FromHtml("#30C1FD");
-            using (LinearGradientBrush brush = new LinearGradientBrush(rec, start, end, 180F)) 
-            { 
-                g.FillRectangle(brush, rec); 
             }
         }
 
         private void btnFechar_Click(object sender, EventArgs e)
-        {
-            DialogResult res = MessageBox.Show("Deseja mesmo sair da aplicação?", "Sair", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
-            if (res == DialogResult.Yes)
-            {
-                Environment.Exit(0);
-            }
-        }
-
-        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
         {
             DialogResult res = MessageBox.Show("Deseja mesmo sair da aplicação?", "Sair", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
             if (res == DialogResult.Yes)
