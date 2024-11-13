1057d686dc8350617ec51cdf7067579901a218a2
PedroWatermann <pedro.programmer.pt@gmail.com>
2024-11-12 22:23:46 -0300

    Aula 12/11/2024

---
+++ Queries/Insert_Tipos.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO [dbo].[Tipos] (nome, descricao)
+VALUES  ('Action Figures e Colecionáveis', 'Figuras detalhadas e estatuetas de personagens icônicos.'),
+		('Consoles e Jogos Eletrônicos', 'Videogames, jogos e acessórios para entretenimento digital com uma variedade de estilos.'),
+		('Jogos de Tabuleiro e RPG', 'Jogos que incentivam interação e criatividade, desde aventuras de RPG até jogos de estratégia.'),
+		('Livros e HQs', 'Publicações com histórias e ilustrações de personagens e universos de ficção.'),
+		('Produtos de Casa e Decoração', 'Artigos decorativos para personalizar ambientes com referências de filmes, séries e jogos.'),
+		('Produtos de Tecnologia', 'Gadgets e acessórios que combinam tecnologia com designs inspirados em temas geek.'),
+		('Roupas e Acessórios', 'Vestuário e acessórios inspirados em franquias da cultura pop.');
--- frmMenu.Designer.cs
+++ frmMenu.Designer.cs
@@ -28,6 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.btnFechar = new System.Windows.Forms.Button();
             this.strMenu = new System.Windows.Forms.MenuStrip();
             this.cadastroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -37,7 +40,9 @@
             this.demonstrativoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.charProdutos = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.strMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.charProdutos)).BeginInit();
             this.SuspendLayout();
             // 
             // btnFechar
@@ -52,9 +57,10 @@
             this.btnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnFechar.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnFechar.ForeColor = System.Drawing.Color.White;
-            this.btnFechar.Location = new System.Drawing.Point(1830, 736);
+            this.btnFechar.Location = new System.Drawing.Point(1665, 702);
+            this.btnFechar.Margin = new System.Windows.Forms.Padding(6);
             this.btnFechar.Name = "btnFechar";
-            this.btnFechar.Size = new System.Drawing.Size(78, 42);
+            this.btnFechar.Size = new System.Drawing.Size(100, 40);
             this.btnFechar.TabIndex = 0;
             this.btnFechar.Text = "Fechar";
             this.btnFechar.UseVisualStyleBackColor = false;
@@ -72,7 +78,8 @@
             this.strMenu.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
             this.strMenu.Location = new System.Drawing.Point(0, 0);
             this.strMenu.Name = "strMenu";
-            this.strMenu.Size = new System.Drawing.Size(1920, 24);
+            this.strMenu.Padding = new System.Windows.Forms.Padding(10, 4, 0, 4);
+            this.strMenu.Size = new System.Drawing.Size(1780, 27);
             this.strMenu.TabIndex = 1;
             this.strMenu.Text = "menuStrip1";
             // 
@@ -83,7 +90,7 @@
             this.produtosToolStripMenuItem});
             this.cadastroToolStripMenuItem.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.cadastroToolStripMenuItem.Name = "cadastroToolStripMenuItem";
-            this.cadastroToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.cadastroToolStripMenuItem.Size = new System.Drawing.Size(66, 19);
             this.cadastroToolStripMenuItem.Text = "Cadastro";
             // 
             // clientesToolStripMenuItem
@@ -102,48 +109,76 @@
             // 
             this.vendasToolStripMenuItem.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.vendasToolStripMenuItem.Name = "vendasToolStripMenuItem";
-            this.vendasToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.vendasToolStripMenuItem.Size = new System.Drawing.Size(56, 19);
             this.vendasToolStripMenuItem.Text = "Vendas";
             // 
             // demonstrativoToolStripMenuItem
             // 
             this.demonstrativoToolStripMenuItem.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.demonstrativoToolStripMenuItem.Name = "demonstrativoToolStripMenuItem";
-            this.demonstrativoToolStripMenuItem.Size = new System.Drawing.Size(97, 20);
+            this.demonstrativoToolStripMenuItem.Size = new System.Drawing.Size(97, 19);
             this.demonstrativoToolStripMenuItem.Text = "Demonstrativo";
             // 
             // sobreToolStripMenuItem
             // 
             this.sobreToolStripMenuItem.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
-            this.sobreToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.sobreToolStripMenuItem.Size = new System.Drawing.Size(49, 19);
             this.sobreToolStripMenuItem.Text = "Sobre";
             // 
             // sairToolStripMenuItem
             // 
             this.sairToolStripMenuItem.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.sairToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
-            this.sairToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(38, 19);
             this.sairToolStripMenuItem.Text = "Sair";
             this.sairToolStripMenuItem.Click += new System.EventHandler(this.btnFechar_Click);
             // 
+            // charProdutos
+            // 
+            this.charProdutos.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.charProdutos.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.Area3DStyle.Enable3D = true;
+            chartArea1.Name = "ChartArea1";
+            this.charProdutos.ChartAreas.Add(chartArea1);
+            this.charProdutos.Location = new System.Drawing.Point(15, 442);
+            this.charProdutos.Margin = new System.Windows.Forms.Padding(6);
+            this.charProdutos.Name = "charProdutos";
+            this.charProdutos.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SemiTransparent;
+            series1.ChartArea = "ChartArea1";
+            series1.Name = "Series1";
+            this.charProdutos.Series.Add(series1);
+            this.charProdutos.Size = new System.Drawing.Size(600, 300);
+            this.charProdutos.TabIndex = 2;
+            this.charProdutos.Text = "chart1";
+            title1.Name = "titProdutos";
+            title1.Text = "Produtos";
+            this.charProdutos.Titles.Add(title1);
+            // 
             // frmMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(11)))), ((int)(((byte)(34)))));
-            this.ClientSize = new System.Drawing.Size(1920, 790);
+            this.ClientSize = new System.Drawing.Size(1780, 757);
+            this.Controls.Add(this.charProdutos);
             this.Controls.Add(this.btnFechar);
             this.Controls.Add(this.strMenu);
+            this.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.White;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MainMenuStrip = this.strMenu;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.Name = "frmMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "frmMenu";
+            this.Text = "Menu";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.strMenu.ResumeLayout(false);
             this.strMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.charProdutos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,5 +195,6 @@
         private System.Windows.Forms.ToolStripMenuItem demonstrativoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.DataVisualization.Charting.Chart charProdutos;
     }
 }
