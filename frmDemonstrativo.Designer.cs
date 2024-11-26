683936a46657d98285f19a80390587295b19585d
Pedro Watermann <pedro.programmer.pt@gmail.com>
2024-11-26 17:56:35 -0300

    frmDemonstrativo iniciado

--- frmDemonstrativo.Designer.cs
+++ frmDemonstrativo.Designer.cs
@@ -28,12 +28,253 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDemonstrativo));
+            this.dgvVenda = new System.Windows.Forms.DataGridView();
+            this.cbxProduto = new System.Windows.Forms.ComboBox();
+            this.lblProduto = new System.Windows.Forms.Label();
+            this.cldDemonstrativo = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.pbxLogo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVenda)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvVenda
+            // 
+            this.dgvVenda.AllowUserToAddRows = false;
+            this.dgvVenda.AllowUserToDeleteRows = false;
+            this.dgvVenda.AllowUserToResizeColumns = false;
+            this.dgvVenda.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(11)))), ((int)(((byte)(33)))));
+            this.dgvVenda.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvVenda.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvVenda.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(11)))), ((int)(((byte)(33)))));
+            this.dgvVenda.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvVenda.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(11)))), ((int)(((byte)(33)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(40)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvVenda.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvVenda.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(11)))), ((int)(((byte)(33)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(188)))), ((int)(((byte)(40)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvVenda.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvVenda.EnableHeadersVisualStyles = false;
+            this.dgvVenda.GridColor = System.Drawing.Color.WhiteSmoke;
+            this.dgvVenda.Location = new System.Drawing.Point(15, 258);
+            this.dgvVenda.Margin = new System.Windows.Forms.Padding(6, 6, 6, 3);
+            this.dgvVenda.Name = "dgvVenda";
+            this.dgvVenda.ReadOnly = true;
+            this.dgvVenda.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(11)))), ((int)(((byte)(33)))));
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(188)))), ((int)(((byte)(40)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvVenda.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvVenda.RowHeadersVisible = false;
+            this.dgvVenda.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dgvVenda.Size = new System.Drawing.Size(570, 196);
+            this.dgvVenda.StandardTab = true;
+            this.dgvVenda.TabIndex = 60;
+            // 
+            // cbxProduto
+            // 
+            this.cbxProduto.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cbxProduto.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.RecentlyUsedList;
+            this.cbxProduto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(11)))), ((int)(((byte)(33)))));
+            this.cbxProduto.Cursor = System.Windows.Forms.Cursors.Default;
+            this.cbxProduto.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cbxProduto.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxProduto.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.cbxProduto.FormattingEnabled = true;
+            this.cbxProduto.Location = new System.Drawing.Point(15, 40);
+            this.cbxProduto.Margin = new System.Windows.Forms.Padding(6, 3, 6, 3);
+            this.cbxProduto.Name = "cbxProduto";
+            this.cbxProduto.Size = new System.Drawing.Size(367, 25);
+            this.cbxProduto.TabIndex = 63;
+            // 
+            // lblProduto
+            // 
+            this.lblProduto.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProduto.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblProduto.Location = new System.Drawing.Point(15, 9);
+            this.lblProduto.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblProduto.Name = "lblProduto";
+            this.lblProduto.Size = new System.Drawing.Size(103, 28);
+            this.lblProduto.TabIndex = 62;
+            this.lblProduto.Text = "Produto";
+            this.lblProduto.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // cldDemonstrativo
+            // 
+            this.cldDemonstrativo.Location = new System.Drawing.Point(15, 84);
+            this.cldDemonstrativo.MaxSelectionCount = 31;
+            this.cldDemonstrativo.Name = "cldDemonstrativo";
+            this.cldDemonstrativo.TabIndex = 66;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(258, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 28);
+            this.label1.TabIndex = 67;
+            this.label1.Text = "De";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(258, 140);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 3, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 28);
+            this.label2.TabIndex = 68;
+            this.label2.Text = "A";
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(11)))), ((int)(((byte)(33)))));
+            this.txtQuantidade.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtQuantidade.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantidade.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.txtQuantidade.Location = new System.Drawing.Point(257, 108);
+            this.txtQuantidade.Margin = new System.Windows.Forms.Padding(6, 3, 6, 3);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(125, 26);
+            this.txtQuantidade.TabIndex = 69;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(11)))), ((int)(((byte)(33)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox1.Location = new System.Drawing.Point(257, 171);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 3, 6, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 26);
+            this.textBox1.TabIndex = 70;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPesquisar.AutoSize = true;
+            this.btnPesquisar.BackColor = System.Drawing.Color.Transparent;
+            this.btnPesquisar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPesquisar.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnPesquisar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnPesquisar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.RoyalBlue;
+            this.btnPesquisar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPesquisar.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPesquisar.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnPesquisar.Image = global::geekStore.Properties.Resources.search_30;
+            this.btnPesquisar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPesquisar.Location = new System.Drawing.Point(257, 206);
+            this.btnPesquisar.Margin = new System.Windows.Forms.Padding(6);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(125, 40);
+            this.btnPesquisar.TabIndex = 71;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnPesquisar.UseVisualStyleBackColor = false;
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.AutoSize = true;
+            this.btnVoltar.BackColor = System.Drawing.Color.Transparent;
+            this.btnVoltar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnVoltar.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnVoltar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.btnVoltar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.btnVoltar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVoltar.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoltar.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnVoltar.Image = global::geekStore.Properties.Resources.back_30;
+            this.btnVoltar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnVoltar.Location = new System.Drawing.Point(471, 206);
+            this.btnVoltar.Margin = new System.Windows.Forms.Padding(12);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(108, 40);
+            this.btnVoltar.TabIndex = 72;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnVoltar.UseVisualStyleBackColor = false;
+            // 
+            // pbxLogo
+            // 
+            this.pbxLogo.Image = global::geekStore.Properties.Resources.geekStoreLogo;
+            this.pbxLogo.Location = new System.Drawing.Point(399, 21);
+            this.pbxLogo.Margin = new System.Windows.Forms.Padding(12);
+            this.pbxLogo.Name = "pbxLogo";
+            this.pbxLogo.Size = new System.Drawing.Size(180, 140);
+            this.pbxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxLogo.TabIndex = 73;
+            this.pbxLogo.TabStop = false;
+            // 
+            // frmDemonstrativo
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(11)))), ((int)(((byte)(33)))));
+            this.ClientSize = new System.Drawing.Size(600, 466);
+            this.Controls.Add(this.pbxLogo);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cldDemonstrativo);
+            this.Controls.Add(this.cbxProduto);
+            this.Controls.Add(this.lblProduto);
+            this.Controls.Add(this.dgvVenda);
+            this.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmDemonstrativo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmDemonstrativo";
+            this.Load += new System.EventHandler(this.frmDemonstrativo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVenda)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvVenda;
+        private System.Windows.Forms.ComboBox cbxProduto;
+        private System.Windows.Forms.Label lblProduto;
+        private System.Windows.Forms.MonthCalendar cldDemonstrativo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.PictureBox pbxLogo;
     }
 }
--- frmDemonstrativo.cs
+++ frmDemonstrativo.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,72 @@ namespace geekStore
 {
     public partial class frmDemonstrativo : Form
     {
+        private readonly SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbGeekStore"].ConnectionString);
+
         public frmDemonstrativo()
         {
             InitializeComponent();
         }
+
+        private void frmDemonstrativo_Load(object sender, EventArgs e)
+        {
+            CarregaCbxProduto();
+            cbxProduto.Focus();
+
+            cldDemonstrativo.MaxDate = DateTime.Now.Date;
+            string sql = "SELECT MIN(dataVenda) FROM Vendas";
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            object minDate = cmd.ExecuteScalar();
+            if (minDate != DBNull.Value && minDate != null)
+            {
+                DateTime minDateTime = Convert.ToDateTime(minDate);
+                cldDemonstrativo.MinDate = minDateTime;
+            }
+            else
+            {
+                MessageBox.Show("Não há registros de vendas ou a tabela está vazia.", "Falha", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                cldDemonstrativo.MinDate = DateTime.Today;
+            }
+
+
+        }
+
+        private void CarregaCbxProduto()
+        {
+            try
+            {
+                string sql = "SELECT Id, nome FROM Produtos ORDER BY nome";
+
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand(sql, con);
+
+                SqlDataAdapter da = new SqlDataAdapter(sql, con);
+
+                DataSet ds = new DataSet();
+
+                da.Fill(ds, "Produtos");
+
+                cbxProduto.ValueMember = "Id";
+                cbxProduto.DisplayMember = "nome";
+                cbxProduto.DataSource = ds.Tables["Produtos"];
+
+                con.Close();
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show($"Ocorreu um erro: {er.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
     }
 }
